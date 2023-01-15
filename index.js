3cc7d970936b5fc2c6decfcbec1a4260e0e6da48
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-15 12:33:26 -0700

    confusing funtions were created

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (driversNames) => {
+  return driversNames.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (driversNames) => {
+  return driversNames.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (farePrice) => {
+  return (multiplier) => multiplier * farePrice;
+};
+
+const fareDoubler = (fare) => {
+  return createFareMultiplier(fare)(2);
+};
+
+const fareTripler = (fare) => {
+  return createFareMultiplier(fare)(3);
+};
+
+const selectDifferentDrivers = (driverArray, selectDrivers) => {
+  return selectDrivers(driverArray);
+};
+
+console.log(
+  selectDifferentDrivers(
+    ["Antonia", "Nuru", "Amari", "Mo"],
+    returnLastTwoDrivers
+  )
+);
+
+console.log(
+  selectDifferentDrivers(
+    ["Antonia", "Nuru", "Amari", "Mo"],
+    returnFirstTwoDrivers
+  )
+);
